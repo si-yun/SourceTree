04d865ae4f29a23ce1f50cfc1a9e2cc376cce100
DESKTOP-P3TTFOB\bit <syun.m2@kakao.com>
2020-08-25 09:46:16 +0900

    develop2

--- src/main/java/bit/ex/Sourcetree/HomeController.java
+++ src/main/java/bit/ex/Sourcetree/HomeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 //develop1
+//develop2
 
 @Controller
 public class HomeController {
